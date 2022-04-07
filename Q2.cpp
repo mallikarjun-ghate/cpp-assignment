796884d55e684f4407e451c4d7386d3e6d32252c
mallikarjun-ghate <99898836+mallikarjun-ghate@users.noreply.github.com>
2022-04-07 14:38:21 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,101 +1,42 @@
-#include<string.h>
 #include <iostream>
+#include <cstring>
 using namespace std;
-
-void getmin(int [],int);
-bool swap(char [],int,int);
-void permutation(char [],int,int);
-int LCSString(string,string,int,int);
-
-int j=0,r=0;
-int a[]={};
-string arr[]={};
-
+int Str = 0;
+int total = 0;
+void check(string input, int len){
+    string temp;
+    for(int i=0;i<len;i++){
+        temp = input.substr(i,2);
+        if(temp=="01"){
+            Str++;
+        }
+    }
+}
 int main()
 {
-   char string[20];
-   int n,k,i,l,m;
-   cout <<"Enter a string: ";
-   cin>>string;
-   
-   int s=strlen(string);
-   
-   n = strlen(string);
-   
-   permutation(string, 0, n);
-   
-   // for(i=0;i<j;i++){
-   //    cout<<arr[i];
-   // }
-   if(j==2){
-      cout<<"The result is 0"<<endl;
-   }
-   else{
-      for(i=0;i<j;i++){
-         for(k=i+1;k<j;k++){
-            //cout << "The length of the LCS is " <<LCSString(arr[i], arr[k], s, s);
-            LCSString(arr[i], arr[k], s, s);
-         }
-      }
-   }
-   getmin(a, r);
-   return 0;
-}
-
-void getmin(int arr[] , int n){
-   int min = arr[0];
-   for(int i = 1; i < n; i++){
-      if(min > arr[i])
-         min = arr[i];
-   }
-   cout<<"\nThe smallest LCS is: "<<min<<endl;
-}
-
-bool swap(char str[], int start, int curr){
- for (int i = start; i < curr; i++)
-    if (str[i] == str[curr])
-       return 0;
-    return 1;
-}
-
-void permutation(char str[], int index, int n){
-    if (index >= n) {
-       //cout<<str<<"\t";
-       arr[j]=str;
-       j++;
-       return;
+    string input;
+    cout<<"Enter A string: ";
+    cin>>input;
+    int len = input.length();
+    check(input,len);
+    cout<<"\nActual string "<<input<<": "<<Str<<endl;
+    total+=Str;
+    Str=0;
+    for(int i=0;i<len-2;i++){
+        for(int j=i+2;j<len;j++){
+            string temp = input;
+            char x;
+            x = temp[i];
+            temp[i] = temp[j];
+            temp[j] = x;
+            if(temp!=input){
+                check(temp,len);
+                cout<<"\nCount of Substring "<<temp<<": "<<Str<<endl;
+                total+=Str;
+                Str=0;
+            }
+        }
     }
-    for (int i = index; i < n; i++) {
-       bool check = swap(str, index, i);
-       if (check) {
-          swap(str[index], str[i]);
-          permutation(str, index + 1, n);
-          swap(str[index], str[i]);
-       }
-    }
- }
-
-int LCSString(string X, string Y, int m, int n)
-{
-   int temp[m + 1][n + 1];
-   int result = 0;
-
-   for (int i = 0; i <= m; i++)
-   {
-      for (int j = 0; j <= n; j++)
-      {
-         if (i == 0 || j == 0)
-            temp[i][j] = 0;
-
-         else if (X[i - 1] == Y[j - 1]) {
-            temp[i][j] = temp[i - 1][j - 1] + 1;
-            result = max(result, temp[i][j]);
-         }
-         else
-            temp[i][j] = 0;
-      }
-   }
-   a[r]=result;
-   r++;
-   return result;
+    cout<<"\nTotal occurence of 01: "<<total<<endl;
+    return 0;
 }
