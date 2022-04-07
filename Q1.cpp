248ec4a203b5ecfd659a719adff76eca92566cd4
mallikarjun-ghate <99898836+mallikarjun-ghate@users.noreply.github.com>
2022-04-07 14:36:59 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,61 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-void swap(char &,char &);
-void find_string(string,int);
-int cur_count=0;
-
+class Text
+{
+public:
+   virtual ~Text(){
+       
+   }
+   virtual void add(char c){ 
+       data += c; 
+   }
+   string getValue(){ 
+       return data; 
+   }
+private:
+   string data;
+};
+class Number : public Text
+{
+   void add(char c)
+   {
+      if (('0' <= 'c') && (c <= '9'))
+         Text::add(c);
+   }
+};
 int main()
 {
-    string str;
-    int total_count = 0;
-    
-    cout<<"Enter a string: ";
-    cin>>str;
-    
-    int len = str.length();
-    
-    find_string(str,len); // check original string has 01 
-    
-    total_count += cur_count; 
-    
-    cur_count=0;
-
-    for(int i=0;i<len-2;i++)
-    {
-        for(int j=i+2;j<len;j+=2){
-            string temp = str;
-            swap(temp[i],temp[j]);
-            if(temp!=str){
-                find_string(temp,len);
-                cout<<temp<<" "; // swapped string
-                //cout<<"count: "<<cur_count<<endl;
-                total_count += cur_count;
-                cur_count = 0;
-            }
-        }
-    }
-
-    cout<<endl<<"Total number of '01' occur in string: " << total_count << endl;
-    return 0;
-}
-
-void swap(char &a,char &b){
-    char temp;
-    temp=a;
-    a=b;
-    b=temp;
-}
-
-void find_string(string inp, int len){
-    string temp;
-    
-    for(int i=0;i<len;i++){
-        temp = inp.substr(i,2);
-
-        if(temp=="01"){
-            cur_count++;
-        }
-    }
+    Text* input = new Number();
+    input->add('1');
+    input->add('a');
+    input->add('0');
+    cout << input->getValue();
 }
